21e2ebc56ed1711f111d71130f6e13c4496c19a2
Dorian.Fournier <Dorian.Fournier@L2621428.production.infra>
2022-03-18 14:18:16 +0100

    Last change before transfer

--- app/src/main/java/com/example/encyclomemedia/MainActivity.kt
+++ app/src/main/java/com/example/encyclomemedia/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.encyclomemedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.viewModels
@@ -12,7 +11,6 @@ import kotlin.random.Random
 class MainActivity : AppCompatActivity() {
 
     private val model: MyViewModel by viewModels()
-    private var i: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,12 +19,12 @@ class MainActivity : AppCompatActivity() {
         val button : Button = findViewById(R.id.button)
         var imageView : ImageView = findViewById(R.id.imageView)
 
+        model.Data.observe(this) {meme ->
+            imageView.load(meme.url)
+        }
+
         button.setOnClickListener{
-            model.setVal(i)
-            i++
-            model.Data.observe(this) {meme ->
-                imageView.load(meme.url)
-            }
+            model.setVal(Random.nextInt(100))
         }
     }
 }
